f291a15c0db2daad927235ef60408f4be079a4b4
shinjaewon99 <tlswodnjs99@naver.com>
2024-08-27 21:24:42 +0900

    feat : HTTP 요청 처리 및 정적 파일 제공 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,16 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,10 +36,42 @@ public class Http11Processor implements Runnable, Processor {
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            final var responseBody = "Hello world!";
+            // HTTP 요청의 첫번째 라인
+            final String[] request = bufferedReader.readLine().split(" ");
+            final String requestTarget = request[1];
 
-            final var response = String.join("\r\n",
+            // "/" 루트 경로일때
+            if (requestTarget.equals("/")) {
+                final var responseBody = "Hello world!";
+
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+            // 루트 경로가 아닐경우
+            URL resource = getClass()
+                    .getClassLoader()
+                    .getResource("static" + requestTarget);
+
+            if (resource == null) {
+                // 리소스를 찾지 못한 경우에 대한 처리
+                throw new FileNotFoundException("Resource not found: " + requestTarget);
+            }
+
+            String filePath = resource.getFile();
+            String responseBody = new String(Files.readAllBytes(Paths.get(filePath)));
+
+
+            final String response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: text/html;charset=utf-8 ",
                     "Content-Length: " + responseBody.getBytes().length + " ",
