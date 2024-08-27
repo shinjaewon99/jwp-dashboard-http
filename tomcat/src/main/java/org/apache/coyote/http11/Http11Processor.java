0019325afcfb7fcdb46556c4b48f248d5559ac6e
shinjaewon99 <tlswodnjs99@naver.com>
2024-08-27 21:34:44 +0900

    refactor : HTTP 요청 처리 기능 메소드 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -13,13 +13,12 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-
+    private static final String HTML_CONTENT_TYPE = "text/html";
+    private static final String CSS_CONTENT_TYPE = "text/css";
     private final Socket connection;
 
     public Http11Processor(final Socket connection) {
@@ -41,47 +40,53 @@ public class Http11Processor implements Runnable, Processor {
             // HTTP 요청의 첫번째 라인
             final String[] request = bufferedReader.readLine().split(" ");
             final String requestTarget = request[1];
-
-            // "/" 루트 경로일때
-            if (requestTarget.equals("/")) {
-                final var responseBody = "Hello world!";
-
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-
-                outputStream.write(response.getBytes());
-                outputStream.flush();
-                return;
-            }
-            // 루트 경로가 아닐경우
-            URL resource = getClass()
-                    .getClassLoader()
-                    .getResource("static" + requestTarget);
-
-            if (resource == null) {
-                // 리소스를 찾지 못한 경우에 대한 처리
-                throw new FileNotFoundException("Resource not found: " + requestTarget);
-            }
-
-            String filePath = resource.getFile();
-            String responseBody = new String(Files.readAllBytes(Paths.get(filePath)));
-
-
-            final String response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            final String response = httpRequestHandler(requestTarget);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String httpRequestHandler(final String requestTarget) throws IOException {
+
+        // "/" 루트 경로일때
+        if (requestTarget.equals("/")) {
+            final var responseBody = "Hello world!";
+
+            return createHttpResponse(requestTarget, responseBody);
+        }
+
+        // 루트 경로가 아닐경우
+        URL resource = getClass()
+                .getClassLoader()
+                .getResource("static" + requestTarget);
+
+        if (resource == null) {
+            // 리소스를 찾지 못한 경우에 대한 처리
+            throw new FileNotFoundException("Resource not found: " + requestTarget);
+        }
+
+        final String filePath = resource.getFile();
+        final String responseBody = new String(Files.readAllBytes(Paths.get(filePath)));
+
+        return createHttpResponse(requestTarget, responseBody);
+    }
+
+    private String createHttpResponse(final String requestTarget, final String responseBody) {
+        return String.join("\r\n",
+                "HTTP/1.1 200 OK ",
+                "Content-Type: " + loadContentType(requestTarget) + ";charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
+    }
+
+    private String loadContentType(final String requestTarget) {
+        if (requestTarget.endsWith(".css")) {
+            return CSS_CONTENT_TYPE;
+        }
+        return HTML_CONTENT_TYPE;
     }
 }
