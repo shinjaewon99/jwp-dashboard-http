ebdd9b6d90ed0b5655d015bfe15b8a8ba93cfcbe
shinjaewon99 <tlswodnjs99@naver.com>
2024-08-28 22:39:02 +0900

    feat : 로그인 기능 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,10 +1,13 @@
 package org.apache.coyote.http11;
 
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
+import nextstep.jwp.model.User;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.security.sasl.AuthenticationException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,6 +18,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -22,6 +26,8 @@ public class Http11Processor implements Runnable, Processor {
     private static final String HTML_CONTENT_TYPE = "text/html";
     private static final String CSS_CONTENT_TYPE = "text/css";
     private static final int INDEX_URI = 1;
+    private static final String QUERY_STRING_ACCOUNT = "account";
+    private static final String QUERY_STRING_PASSWORD = "password";
     private final Socket connection;
 
     public Http11Processor(final Socket connection) {
@@ -61,6 +67,34 @@ public class Http11Processor implements Runnable, Processor {
             return createHttpResponse(requestTarget, responseBody);
         }
 
+        // /login 경로 일때
+        if(requestTarget.equals("/login")){
+            Map<String, String> parseQueryString = parseQueryString(requestTarget);
+            User user = findAccount(parseQueryString);
+
+            boolean checkedPassword = user.checkPassword(parseQueryString.get(QUERY_STRING_PASSWORD));
+
+            // 비밀번호가 불일치 할경우
+            if(!checkedPassword){
+                log.info("PASSWORD 불일치" + user.getAccount());
+                throw new AuthenticationException();
+            }
+
+            final String responseBody = createUrlResource(requestTarget);
+            return createHttpResponse(requestTarget, responseBody);
+        }
+
+        final String responseBody = createUrlResource(requestTarget);
+
+        return createHttpResponse(requestTarget, responseBody);
+    }
+
+    private User findAccount(Map<String, String> parseQueryString) {
+        String findAccount = parseQueryString.get(QUERY_STRING_ACCOUNT);
+        return InMemoryUserRepository.findByAccount(findAccount).orElseThrow(NoSuchElementException::new);
+    }
+
+    private String createUrlResource(String requestTarget) throws IOException {
         // 루트 경로가 아닐경우
         final URL resource = getClass()
                 .getClassLoader()
@@ -72,9 +106,7 @@ public class Http11Processor implements Runnable, Processor {
         }
 
         final String filePath = resource.getFile();
-        final String responseBody = new String(Files.readAllBytes(Paths.get(filePath)));
-
-        return createHttpResponse(requestTarget, responseBody);
+        return new String(Files.readAllBytes(Paths.get(filePath)));
     }
 
     private String createHttpResponse(final String requestTarget, final String responseBody) {
@@ -93,7 +125,7 @@ public class Http11Processor implements Runnable, Processor {
         return HTML_CONTENT_TYPE;
     }
 
-     static Map<String, String> parseQueryString(final String requestTarget) {
+    private Map<String, String> parseQueryString(final String requestTarget) {
         // /search?q=java&sort=ascending
         // q가 key, java가 value
         final Map<String, String> queryParams = new HashMap<>();
@@ -123,3 +155,4 @@ public class Http11Processor implements Runnable, Processor {
         return queryParams;
     }
 }
+
