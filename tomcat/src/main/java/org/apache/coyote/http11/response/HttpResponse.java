df0a1a376bf6feed5bced843deb443f7c2c56fe5
shinjaewon99 <tlswodnjs99@naver.com>
2024-08-31 09:36:04 +0900

    refactor : Getter 어노테이션 추가

--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponse.java
@@ -1,5 +1,8 @@
 package org.apache.coyote.http11.response;
 
+import lombok.Getter;
+
+@Getter
 public class HttpResponse {
 
     private static final String HTML_CONTENT_TYPE = "text/html";
@@ -20,14 +23,19 @@ public class HttpResponse {
                 responseBody));
     }
 
+    public static HttpResponse from(final HttpResponseEntity httpResponse) {
+        final HttpStatus httpStatus = httpResponse.getHttpStatus();
+        final String fullHttpStatus = httpStatus.getHttpStatusCode() + " " + httpStatus.name();
+        final String requestTarget = httpResponse.getRequestTarget();
+        final String responseBody = httpResponse.getResponseBody();
+
+        return HttpResponse.of(fullHttpStatus, requestTarget, responseBody);
+    }
+
     private static String loadContentType(final String requestTarget) {
         if (requestTarget.endsWith(".css")) {
             return CSS_CONTENT_TYPE;
         }
         return HTML_CONTENT_TYPE;
-    }
-
-    public String getHttpResponse() {
-        return httpResponse;
     }
 }
