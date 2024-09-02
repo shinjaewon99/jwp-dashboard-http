8841ae41975458f0b1f8263c623eca7c0c8a2f54
shinjaewon99 <tlswodnjs99@naver.com>
2024-09-02 21:13:31 +0900

    feat : HTTP 쿠키 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/cookie/HttpCookie.java
@@ -0,0 +1,20 @@
+package org.apache.coyote.http11.cookie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpCookie {
+
+    private static final String SEPARATOR = "; ";
+    private static final String DELIMITER = "=";
+
+    private final Map<String, String> cookies;
+
+    public HttpCookie() {
+        this.cookies = new HashMap<>();
+    }
+
+    public String findCookieValue(final String cookieKey) {
+        return cookies.get(cookieKey);
+    }
+}
