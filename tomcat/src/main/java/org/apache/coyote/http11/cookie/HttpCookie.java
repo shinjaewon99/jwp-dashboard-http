e998deb78c6226f0a025a3ac6087d7c821efa17a
shinjaewon99 <tlswodnjs99@naver.com>
2024-09-03 21:15:03 +0900

    feat : HTTP 쿠키에 대한 파싱 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/cookie/HttpCookie.java
+++ tomcat/src/main/java/org/apache/coyote/http11/cookie/HttpCookie.java
@@ -10,8 +10,25 @@ public class HttpCookie {
 
     private final Map<String, String> cookies;
 
-    public HttpCookie() {
+    public HttpCookie(Map<String, String> cookies) {
+        this.cookies = new HashMap<>(cookies);
+    }
+
+    private HttpCookie() {
         this.cookies = new HashMap<>();
+    }
+
+    public static HttpCookie from(final String cookie) {
+        Map<String, String> cookiesStore = new HashMap<>();
+        String[] lines = cookie.split(SEPARATOR);
+
+        for (String line : lines) {
+            String[] keyValue = line.split(DELIMITER);
+
+            cookiesStore.put(keyValue[0], keyValue[1]);
+        }
+
+        return new HttpCookie(cookiesStore);
     }
 
     public String findCookieValue(final String cookieKey) {
