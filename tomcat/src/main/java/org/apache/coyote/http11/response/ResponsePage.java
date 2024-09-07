43613b9f0567558b54904a069e5dff19d89eb468
shinjaewon99 <tlswodnjs99@naver.com>
2024-09-07 21:21:58 +0900

    feat : 빈페이지를 위한 Empty 추가

--- tomcat/src/main/java/org/apache/coyote/http11/response/ResponsePage.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ResponsePage.java
@@ -7,8 +7,8 @@ public enum ResponsePage {
     LOGIN_PAGE_URI("/login.html"),
     UNAUTHORIZED_PAGE_URI("/401.html"),
     INDEX_PAGE_URI("/index.html"),
-    REGISTER_PAGE_URI("/register.html");
-
+    REGISTER_PAGE_URI("/register.html"),
+    EMPTY("");
     private final String htmlUri;
 
     ResponsePage(String htmlUri) {
