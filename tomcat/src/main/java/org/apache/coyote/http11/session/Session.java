a8713144dca9be6812b86982a81ac23cfc236e82
shinjaewon99 <tlswodnjs99@naver.com>
2024-09-09 21:18:40 +0900

    feat : 세션을 비우는 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/session/Session.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/Session.java
@@ -5,9 +5,22 @@ import java.util.Map;
 
 public class Session {
 
+    private final String id;
     private final Map<String, String> session = new HashMap<>();
+
+    public Session(final String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 
     public void setSession(final String name, final String value) {
         session.put(name, value);
+    }
+
+    public void clearSession() {
+        session.clear();
     }
 }
