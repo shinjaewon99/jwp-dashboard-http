b97a3a6e6be745bb57a00edfc159cc3528447fbc
shinjaewon99 <tlswodnjs99@naver.com>
2024-09-10 21:40:38 +0900

    test : 로그인 실패 테스트

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/LoginTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/LoginTest.java
@@ -72,4 +72,37 @@ public class LoginTest {
 
         assertThat(socket.output()).contains(expected);
     }
+
+    @Test
+    void 로그인_실패_테스트() throws IOException {
+
+        // given
+        final String requestBody = "account=shin&password=pass123";
+
+        final String httpRequest = String.join("\r\n",
+                "POST /login HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "Content-Length: " + requestBody.getBytes().length,
+                "",
+                requestBody);
+
+        final var socket = new StubSocket(httpRequest);
+        final Http11Processor processor = new Http11Processor(socket);
+
+        // when
+        processor.process(socket);
+
+        // then
+        final URL resource = getClass().getClassLoader().getResource("static/401.html");
+        final String responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+        var expected = "HTTP/1.1 401 UNAUTHORIZED \r\n" +
+                "Content-Type: text/html;charset=utf-8 \r\n" +
+                "Content-Length: " + responseBody.getBytes().length + " \r\n" +
+                "\r\n" +
+                responseBody;
+
+        assertThat(socket.output()).contains(expected);
+    }
 }
