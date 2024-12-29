f811b48253fbf5a4112cc32bd4fab78b7d54c55b
shinjaewon99 <tlswodnjs99@naver.com>
2024-12-30 08:51:22 +0900

    feat : 매직넘버 상수화 처리

--- tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestStartLine.java
+++ tomcat/src/main/java/org/apache/coyote/http11/request/HttpRequestStartLine.java
@@ -11,9 +11,12 @@ import java.util.stream.Collectors;
 
 @Getter
 public class HttpRequestStartLine {
-
-    private static final Logger log = LoggerFactory.getLogger(HttpRequestStartLine.class);
     private static final String BLANK = " ";
+    private static final int HTTP_METHOD_INDEX = 0;
+    private static final int PATH_INDEX = 1;
+    private static final int HTTP_VERSION = 2;
+    private static final int REQUEST_START_LINE_LENGTH = 3;
+
     private final HttpMethod httpMethod;
     private final String path;
     private final String httpVersion;
@@ -29,13 +32,13 @@ public class HttpRequestStartLine {
         validateHttpRequestStartLineSize(requestStartLine);
 
         return new HttpRequestStartLine(
-                HttpMethod.of(requestStartLine.get(0)),
-                requestStartLine.get(1),
-                requestStartLine.get(2));
+                HttpMethod.of(requestStartLine.get(HTTP_METHOD_INDEX)),
+                requestStartLine.get(PATH_INDEX),
+                requestStartLine.get(HTTP_VERSION));
     }
 
     private static void validateHttpRequestStartLineSize(final List<String> requestStartLine) {
-        if (requestStartLine.size() != 3) {
+        if (requestStartLine.size() != REQUEST_START_LINE_LENGTH) {
             throw new IllegalStateException();
         }
     }
