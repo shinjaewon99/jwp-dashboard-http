c9b02a82f4983882cb41635f1f47519e73b0f4ab
shinjaewon99 <tlswodnjs99@naver.com>
2025-01-13 15:50:53 +0900

    feat : Main 페이지 컨트롤러 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/IndexPageController.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11.controller;
+
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.*;
+
+import java.io.IOException;
+
+import static org.apache.coyote.http11.response.ResponsePage.EMPTY;
+
+public class IndexPageController implements Controller {
+
+
+    @Override
+    public HttpResponseEntity service(final HttpRequest httpRequest, final HttpResponse httpResponse) throws IOException {
+        final String requestTarget = httpRequest.getHttpRequestStartLine().getPath();
+
+        final var responseBody = "Hello world!";
+
+        return HttpResponseEntity
+                .builder()
+                .httpStatus(HttpStatus.OK)
+                .contentType(generateContentType(requestTarget))
+                .responseBody(HttpResponseBody.from(responseBody))
+                .responsePage(EMPTY)
+                .build();
+    }
+}
