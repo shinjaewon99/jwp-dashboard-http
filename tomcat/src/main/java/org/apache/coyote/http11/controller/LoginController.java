7eedbdf685962d443e2c1a280174868f9e907831
shinjaewon99 <tlswodnjs99@naver.com>
2025-01-19 20:30:27 +0900

    feat : Login 컨트롤러 추가

---
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
@@ -0,0 +1,87 @@
+package org.apache.coyote.http11.controller;
+
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
+import org.apache.coyote.http11.request.HttpMethod;
+import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.request.HttpRequestBody;
+import org.apache.coyote.http11.request.HttpRequestStartLine;
+import org.apache.coyote.http11.response.HttpResponseEntity;
+import org.apache.coyote.http11.response.HttpStatus;
+import org.apache.coyote.http11.session.JSessionIdGenerator;
+import org.apache.coyote.http11.session.Session;
+import org.apache.coyote.http11.session.SessionManager;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import static org.apache.coyote.http11.response.ResponsePage.*;
+
+public class LoginController implements Controller {
+    private static final String ACCOUNT_FIELD = "account";
+    private static final String PASSWORD_FIELD = "password";
+    private final SessionManager sessionManager = new SessionManager();
+
+    @Override
+    public HttpResponseEntity service(final HttpRequest httpRequest) throws IOException {
+        HttpRequestStartLine httpRequestStartLine = httpRequest.getHttpRequestStartLine();
+
+        HttpRequestBody httpRequestBody = httpRequest.getHttpRequestBody();
+        HttpMethod httpMethod = httpRequestStartLine.getHttpMethod();
+        String requestTarget = httpRequestStartLine.getPath();
+        String account = httpRequestBody.findBodyValue(ACCOUNT_FIELD);
+
+        if (httpMethod == HttpMethod.GET && account == null) {
+            return HttpResponseEntity
+                    .builder()
+                    .httpStatus(HttpStatus.OK)
+                    .contentType(generateContentType(requestTarget))
+                    .responsePage(LOGIN_PAGE_URI)
+                    .build();
+        }
+
+        final User user = findAccount(account);
+        final String password = httpRequestBody.findBodyValue(PASSWORD_FIELD);
+
+        boolean checkedPassword = user.checkPassword(password);
+
+        // 비밀번호가 불일치 할경우
+        if (!checkedPassword) {
+            return handleLoginFail(requestTarget);
+        }
+
+        return handleLoginSuccess(requestTarget);
+    }
+
+    private HttpResponseEntity handleLoginSuccess(final String requestTarget) {
+
+        HttpResponseEntity httpResponseEntity = HttpResponseEntity
+                .builder()
+                .httpStatus(HttpStatus.FOUND)
+                .contentType(generateContentType(requestTarget))
+                .responsePage(INDEX_PAGE_URI)
+                .build();
+
+        final String jSessionId = JSessionIdGenerator.generateSessionId();
+        httpResponseEntity.setCookie("JSESSIONID", jSessionId);
+
+        // Session에 쿠키 담기
+        Session session = new Session(jSessionId);
+        sessionManager.add(session);
+
+        return httpResponseEntity;
+    }
+
+    private HttpResponseEntity handleLoginFail(final String requestTarget) {
+        return HttpResponseEntity
+                .builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED)
+                .contentType(generateContentType(requestTarget))
+                .responsePage(UNAUTHORIZED_PAGE_URI)
+                .build();
+    }
+
+    private User findAccount(final String account) {
+        return InMemoryUserRepository.findByAccount(account).orElseThrow(NoSuchElementException::new);
+    }
+}
